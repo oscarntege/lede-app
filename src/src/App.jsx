33ba0456bc5f8751ee9ca340d298d04decbae545
oscarntege <oscanog@gmail.com>
2026-02-19 21:05:33 +0300

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,7 @@
+const CONFIG = {
+  STRIPE_PAYMENT_LINK: "https://buy.stripe.com/YOUR_LINK_HERE",
+  MOBILE_MONEY_LINK: "https://flutterwave.com/pay/YOUR_LINK_HERE",
+  WHATSAPP_SUPPORT: "https://wa.me/256701690711",
+  PRICE: "$49",
+  PRICE_UGX: "180,000 UGX",
+};
